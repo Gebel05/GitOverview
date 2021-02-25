2973feb8a47590de2b4bc5e940d695a74aedceab
Alen Zhebel <gebelav05gmail.com>
2021-02-25 23:20:36 +0300

    new Class

--- src/sss.java
+++ src/sss.java
@@ -41,6 +41,7 @@ public class sss {
 
         Stack mystack1 = new Stack();
         Stack mystack2 = new Stack();
+        Stack mystack3 = new Stack();
 
 
        for (int i = 0; i<10; i++) mystack1.push(i);
